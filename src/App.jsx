c276307204830619922fcbbbcae387fe1c6ba456
Виталий <dudkin231@mail.ru>
2025-11-21 10:57:36 +0300

    added improvments to app.css app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header/Header';
-import PopExit from './components/PopExit/PopExit';
-import PopNewCard from './components/PopNewCard/PopNewCard'; 
-import PopBrowse from './components/PopBrowse/PopBrowse';
-import Main from './components/Main/Main';
+// src/App.jsx
 
+import React from 'react';
+import AppRoutes from './AppRoutes';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="wrapper">
-
-      {/* Pop-up: Выйти из аккаунта */}
-      <PopExit/>
-
-      {/* Pop-up: Создание задачи */}
-      <PopNewCard />
-
-      {/* Pop-up: Просмотр задачи */}
-      <PopBrowse />
-
-      {/* Основной контент */}
-      <Header/>
-
-       <Main /> 
-    </div>
-  );
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
