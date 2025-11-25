af8b85fd788abfc6d4c70447942522dd9ac1a8b6
Виталий <dudkin231@mail.ru>
2025-11-25 13:00:22 +0300

    added routers

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from 'react-router-dom';
 import MainBoardPage from './pages/MainBoardPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import BrowseTaskPage from './pages/BrowseTaskPage';
+import ExitPage from './pages/ExitPage';
 import NotFoundPage from './pages/NotFoundPage';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -20,10 +22,14 @@ export default function AppRoutes() {
 
   return (
     <Routes>
-      <Route
-        element={<PrivateRoute isAuth={isAuth} />}
-      >
-        <Route path="/" element={<MainBoardPage loading={loading} />} />
+      <Route element={<PrivateRoute isAuth={isAuth} />}>
+        <Route path="/" element={<MainBoardPage />} />
+        <Route
+          path="/task/new"
+          element={<MainBoardPage withNewTaskModal={true} />}
+        />
+        <Route path="/task/:id" element={<MainBoardPage withBrowseTaskId={true} />} />
+        <Route path="/exit" element={<ExitPage />} />
       </Route>
       <Route path="/login" element={<LoginPage setIsAuth={setIsAuth} />} />
       <Route path="/register" element={<RegisterPage setIsAuth={setIsAuth} />} />
